008e4624714eb97a295e0addced7b787a0e1fca7
Adryellsales <adryellsales@hotmail.com>
2023-06-22 08:39:23 -0300

    Criacao de tamanhos randomicos para as moscas

--- jogo.js
+++ jogo.js
@@ -11,8 +11,8 @@ ajustaTamanhoPalcoJogo()
 
 
 function posicaoRandomica() {
-    var posicaoX = Math.floor(Math.random() * largura) -90
-    var posicaoY = Math.floor(Math.random() * altura) -90
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
 
     posicaoX = posicaoX < 0 ? 0 : posicaoX
     posicaoY = posicaoY < 0 ? 0 : posicaoY
@@ -22,10 +22,27 @@ function posicaoRandomica() {
     // Criar o elemento HTML 
     var mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosca.png'
-    mosquito.className = "mosquito1"
+    mosquito.className = tamanhoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
 
     document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+            break
+        case 1:
+            return 'mosquito2'
+            break
+        case 2:
+            return 'mosquito3'
+            break
+    }
+
 }
